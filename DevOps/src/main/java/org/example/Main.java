8ac358e9b1fa38ce8e716575a1ecf32faa895ec9
Mirko Petrovic <66567615+miirko99@users.noreply.github.com>
2022-09-27 11:26:34 +0200

    Update Main.java

--- DevOps/src/main/java/org/example/Main.java
+++ DevOps/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //komentarr
+        //komentar
         System.out.println("Hello world!");
     }
 }
